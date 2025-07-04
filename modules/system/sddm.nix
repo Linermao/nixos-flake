4b551fc4ef993d83f39d8bd8a3a561c007714f62
Linermao <LinermaoGemail@gmail.com>
2025-07-04 10:18:24 +0800

    sddm

--- modules/system/sddm.nix
+++ modules/system/sddm.nix
@@ -4,6 +4,19 @@
   services.displayManager = {
     sddm.enable = true;
     sddm.theme = "pixel-sakura";
+    sddm.wayland.enable = false;
   };
+
   environment.etc."sddm/themes/pixel-sakura".source = "${paths.resources}/sddm-theme";
+
+  fonts = {
+    fontDir.enable = true;
+    packages = [
+      (pkgs.runCommand "arcadeclassic-font" {} ''
+        mkdir -p $out/share/fonts/truetype
+        cp ${paths.resources}/sddm-theme/Fonts/ARCADECLASSIC.ttf $out/share/fonts/truetype/
+      '')
+    ];
+  };
+  fonts.fontDir.enable = true;
 }
